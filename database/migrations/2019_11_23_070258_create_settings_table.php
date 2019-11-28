873c842fac6c3d72e42623b2aebe4817d41de29f
ram <ram196kumar@gmail.com>
2019-11-28 19:51:57 +0530

    delted migration file

--- database/migrations/2019_11_23_070258_create_settings_table.php
+++ database/migrations/2019_11_23_070258_create_settings_table.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-
-
 class CreateSettingsTable extends Migration
 {
     /**
